bd50960e6fc57ed1d4938defdaa401f86260e28b
TDiva <tanushadomanova@gmail.com>
2013-09-12 14:16:12 +0300

    Uploading file to server

--- catrobatWebsite/src/main/java/org/catrobat/catroid/HomeController.java
+++ catrobatWebsite/src/main/java/org/catrobat/catroid/HomeController.java
@@ -1,44 +1,29 @@
 package org.catrobat.catroid;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Model model) {
+
 		return "home";
 	}
-	
+
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public void Upload() {
-		System.out.println("hi!");
+	public String Upload(
+			@RequestParam(value = "file", required = true) MultipartFile file)
+			throws IOException {
+		System.out.println(file.getOriginalFilename());
+		return "home";
 	}
-	
+
 }
--- catrobatWebsite/src/main/webapp/WEB-INF/views/home.jsp
+++ catrobatWebsite/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,36 +1,12 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page session="false"%>
 <html>
+<%@include file="scripts.jspf"%>
 <head>
 <title>Home</title>
-<script type="text/javascript"
-	src="http://scriptjava.net/source/scriptjava/scriptjava.js"></script>
-<script type="text/javascript">
-	function SendFile() {
-		$$f({
-			formid : 'upload_form',
-			url : 'upload',
-			onstart : function() {
-				$$('result', 'start uploading');
-			},
-			onsend : function() {
-				$$('result', $$('result').innerHTML + '<br />success');
-			}
-		});
-	}
-</script>
 </head>
 <body>
 	<h1>Hello world!</h1>
-	<form id="upload_form" method="post" enctype="multipart/form-data"
-		onSubmit="">
-		<input class="file_form" type="file" name="upload_file" />
-	</form>
-	<br />
-	<div id="result">Status</div>
-	<br />
-	<br />
-	<div onclick="SendFile();">Upload file</div>
-	<br />
+	<%@include file="upload.jspf"%>
 </body>
 </html>
